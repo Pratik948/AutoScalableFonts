463bfdad6bf584de35f9171c141db90d9a4cbd2e
Pratik948 <jamariyapratik@gmail.com>
2020-10-21 22:38:21 +0530

    Re-added framework target

---
+++ AutoScalableFonts/AutoScalableFonts.h
@@ -0,0 +1,18 @@
+//
+//  AutoScalableFonts.h
+//  AutoScalableFonts
+//
+//  Created by Hubilo Softech Private Limited on 21/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoScalableFonts.
+FOUNDATION_EXPORT double AutoScalableFontsVersionNumber;
+
+//! Project version string for AutoScalableFonts.
+FOUNDATION_EXPORT const unsigned char AutoScalableFontsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoScalableFonts/PublicHeader.h>
+
+
